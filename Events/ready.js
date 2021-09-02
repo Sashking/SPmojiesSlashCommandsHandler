41da6244f0ea940d3652a2e7e91ada31e73f6899
Sashking <sashking.cz@gmail.com>
2021-09-02 18:13:49 +0200

    Added the right activity

--- Events/ready.js
+++ Events/ready.js
@@ -1,5 +1,6 @@
 const client = require("../index");
 
 client.on("ready", () => {
+    client.user.setActivity('vk.com/spmojies', { type: 'WATCHING' });
     console.log(`✅ ${client.user.username}`);
 });
