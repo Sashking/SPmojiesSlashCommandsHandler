fbaecddc4bf6010c8db99c824de14c9733ad4324
Sashking <sashking.cz@gmail.com>
2021-10-03 11:38:53 +0200

    Quick ping command + ping choice

--- Commands/Admin/quick-ping.js
+++ Commands/Admin/quick-ping.js
@@ -10,6 +10,22 @@ module.exports = {
             type: 'CHANNEL',
             required: true
         },
+        {
+            name: 'пинг',
+            description: 'Выберите какой пинг вы хотите использовать',
+            type: 'STRING',
+            required: true,
+            choices: [
+                {
+                    name: '@everyone',
+                    value: 'everyone'
+                },
+                {
+                    name: 'Оповещения',
+                    value: 'notifications'
+                },
+            ]
+        },
     ],
     userPermissions: ['ADMINISTRATOR'],
 
@@ -20,11 +36,13 @@ module.exports = {
      */
     run: async(client, interaction, args) => {
 
-        const [ channelId ] = args;
+        const [ channelId, pingType ] = args;
 
         const targetChannel = interaction.guild.channels.cache.get(channelId);
+        let ping = "<@&842438141646340146>";
+        if (pingType == "everyone") ping = "@everyone";
 
-        targetChannel.send({ content: '<@&842438141646340146>' })
+        targetChannel.send({ content: ping })
             .then(m => {
                 m.delete();
             });
